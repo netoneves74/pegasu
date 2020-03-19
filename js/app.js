a4cca127c7556d19fb7094ec58346e2fc2dce960
netoneves74 <netonevesgbi@gmail.com>
2020-03-19 14:58:23 -0300

    Add menu mobille e ajustes na pág contatos

--- js/app.js
+++ js/app.js
@@ -1,32 +1,19 @@
 $('.nav a').click(function(e) {
-    e.preventDefault();
     var id = $(this).attr('href'),
         targetOffset = $(id).offset().top;
 
     $('html, body').animate({
         scrollTop: targetOffset
     }, 500);
-
+    e.preventDefault();
 });
 
 
 // Menu Sanduiche
 
-$('.menu-anchor').on('click touchstart', function(e) {
-    $(this).toggleClass('menu-anchor-active');
-    $('.feather-menu').toggleClass('parse-white');
+$('.sdw-menu').on('click touchstart', function(e) {
+    $(this).toggleClass('sdw-menu-active');
+    $('.nav').toggleClass('nav-active');
     $('.menu').toggleClass('menu-active');
-    e.preventDefault();
-});
-
-$('.busca-anchor').on('click touchstart', function(e) {
-    $('.campo-busca').toggleClass('busca-anchor-active');
-    $('.close-anchor').toggleClass('close-anchor-active');
-    e.preventDefault();
-});
-
-$('.close-anchor').on('click touchstart', function(e) {
-    $(this).toggleClass('close-anchor-active');
-    $('.campo-busca').toggleClass('busca-anchor-active');
     e.preventDefault();
 });
